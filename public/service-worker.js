9fff67b4e05affd8fb2e9e1feb0713b296dfa412
jordidaigle <jdaigle992@gmail.com>
2020-08-11 18:33:21 -0500

    changed file paths in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,11 @@
-const FILES_TO_CACHE = ["/", 
-"/index.html", 
-"/index.js", 
-"/styles.css",
+const FILES_TO_CACHE = [
+"/", 
+"./index.html", 
+"./index.js", 
+"./styles.css",
 // "/db.js",
-"/icons/icon-192x192.png",
-"/icons/icon-512x512.png"
+"./icons/icon-192x192.png",
+"./icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = "static-cache-v2";
