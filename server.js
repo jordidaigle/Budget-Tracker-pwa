c45674081735511f83853c4d441931d7ba3e9890
jordidaigle <jdaigle992@gmail.com>
2020-08-11 19:04:11 -0500

    deploying for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -24,6 +24,9 @@ mongoose
 
 // routes
 app.use(require("./routes/api.js"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
